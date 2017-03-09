d7e06e0a33c6848857000f51c438b98c8b68e04a
Jacky Fong <jacky@KaliMac.local>
2017-03-10 00:08:22 +0100

    Init commit

---
+++ src/components/Intro.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="intro">
+    <img src="../assets/logo.png">
+    <h1></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+}
+
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Intro from '@/components/Intro'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    { path: '/', name: 'intro', component: Intro}
+  ]
+})
