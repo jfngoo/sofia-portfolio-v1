54bba8588b9f2ccd1d72582492fe010d95b0d198
Sofia Boggio <boggio.sofia@gmail.com>
2017-04-10 17:32:49 +0200

    Remplissage des différents projets

--- src/data/index.js
+++ src/data/index.js
@@ -5,48 +5,47 @@ export const data_en =
       title: "Cushy!",
       type: "IoT",
       date: "2017",
-      background: "lost-elements.png",
+      background: "cushy-cover.jpg",
       details: {
         what: ["IoT", "Arduino"],
         when: "2017",
         who: ["Léo Ternoir", "Jacky Fong", "Léna Scaravella", "Thomas Boulongne", "Sofia Boggio"]
       },
       text1: "Cushy! is a cushion that you can squeeze, pet, or punch when you’re cozy, on your couch, and you don’t know what TV show or movie to watch. It’ll tell you what’s best based on your mood. No need to wonder around for hours, looking for something you haven’t seen and that seems to fit how you’re feeling tonight.",
-      vimeoId: "211794682",
-      text2: "Cushy! is a cushion that you can squeeze, pet, or punch when you’re cozy, on your couch, and you don’t know what TV show or movie to watch. It’ll tell you what’s best based on your mood. No need to wonder around for hours, looking for something you haven’t seen and that seems to fit how you’re feeling tonight.",
-      photos: ["placeholder.jpg", "placeholder2.jpg", "placeholder3.jpg", "placeholder4.jpg"]
+      vimeoId: "202369823",
+      text2: "By pressing, stroking, cuddling or even punching Cushy, he'll know and tell you what movie correspond to your mood, based on your gestures. Thanks to a user friendly interface, you can get to your movie or TV show very fast. More here http://cushy.jackyfong.com/",
+      photos: ["cushy-1.jpg", "cushy-2.jpg", "cushy-3.jpg", "cushy-4.png", "cushy-5.gif"]
     },
     {
       id: "lost-elements",
       title: "Lost Elements",
       type: "Tablet Game",
       date: "2016",
-      background: "lost-elements.png",
+      background: "lostelements-cover.jpg",
       details: {
         what: ["Tablet game", "IoT", "Unity"],
         when: "2016",
         who: ["Anthelme Dumont", "Mélanie Lo", "Kirgan Somville", "Sofia Boggio"]
       },
-      text1: "Oui oui",
+      text1: "Climatic issues are complex and linked together. In general, even if significant pourcentage of the population feels concerned by global warming, it seems hard to fully grasp this subject since medias and society tend to give a simplistic overview of it all. Therefor, we focused on climatic and environmental issues, and more precisely the understanding of what’s at stake.",
       vimeoId: "173657249",
-      text2: "Non non.",
-      photos: ["le-1.png", "le-2.png", "le-3.png", "le-4.png", "le-5.png"]
+      text2: "''At the dawn of time, the four fundamental elements couldn’t be controlled. Water, sun, wind, and earth unleashed upon the world, devastating everything in their path. Though all those elements were essential for our planet to sustain any life, it is said that an entity named Anjä was born in the depths of the Earth. She had the daunting task of containing the elements in an ancient relic and becoming its guardian. Century by century, nature finally calmed down. So Anjä slowly disappeared, conscientiously/carefully leaving the relic hidden inside a temple. But she hadn’t planned on humankind appearing… and stealing the relic for their own purposes.''",
+      photos: ["lostelements-1.jpg", "lostelements-2.jpg", "lostelements-4.jpg", "lostelements-3.png", "lostelements-5.jpg", "lostelements-6.jpg"]
     },
     {
       id: "orianna",
       title: "Orianna",
       type: "Cosplay",
       date: "2015",
-      background: "lost-elements.png",
+      background: "orianna-cover.jpg",
       details: {
-        what: ["IoT", "Arduino"],
-        when: "2017",
-        who: ["Léo Ternoir", "Jacky Fong", "Léna Scaravella", "Thomas Boulongne", "Sofia Boggio"]
+        what: ["Cosplay", "Arduino"],
+        when: "2015",
+        who: ["Sofia Boggio", "help from Philippe Ravaux", "and Sarah Goldberg"]
       },
-      text1: "Cushy! is a cushion that you can squeeze, pet, or punch when you’re cozy, on your couch, and you don’t know what TV show or movie to watch. It’ll tell you what’s best based on your mood. No need to wonder around for hours, looking for something you haven’t seen and that seems to fit how you’re feeling tonight.",
+      text1: "The weird “hobby” that is Cosplay brought me to experiment with two Arduino Uno and several motors (more that I could count). I even made the rail of the skirt, that allows it to turn, with a 3D printer. A lot of time was spent thinking of the right mecanisms for each part of this costume. This whole costume was handmade.",
       youtubeId: "XXzo9GRlFNo",
-      text2: "Cushy! is a cushion that you can squeeze, pet, or punch when you’re cozy, on your couch, and you don’t know what TV show or movie to watch. It’ll tell you what’s best based on your mood. No need to wonder around for hours, looking for something you haven’t seen and that seems to fit how you’re feeling tonight.",
-      photos: ["o-3.gif", "o-4.jpg", "o-5.jpg"]
+      photos: ["o-3.gif", "o-4.jpg", "o-4bis.jpg", "o-5.jpg", "o-7.jpg", "o-8.jpg", "o-9.jpg"]
     },
     {
       id: "s4m-m1",
@@ -56,28 +55,27 @@ export const data_en =
       background: "sammi.png",
       details: {
         what: ["IoT", "Arduino"],
-        when: "2017",
-        who: ["Léo Ternoir", "Jacky Fong", "Léna Scaravella", "Thomas Boulongne", "Sofia Boggio"]
+        when: "2014",
+        who: ["Cynthia Charles", "Sofia Boggio"]
       },
-      text1: "Cushy! is a cushion that you can squeeze, pet, or punch when you’re cozy, on your couch, and you don’t know what TV show or movie to watch. It’ll tell you what’s best based on your mood. No need to wonder around for hours, looking for something you haven’t seen and that seems to fit how you’re feeling tonight.",
+      text1: "Both technologie's lovers, curious and passionate by Arduino, we couldn't miss this opportunity to use it. But, as we never had the chance the try, we couldn't either set the bar too high. Therefor, based on many inspirations, we've created S4M-M1 aka Sammy, a small Robot, autonomous thanks to his HC SR-04 sensor ; conceived / made / programmed by us.",
       vimeoId: "123190477",
-      text2: "Cushy! is a cushion that you can squeeze, pet, or punch when you’re cozy, on your couch, and you don’t know what TV show or movie to watch. It’ll tell you what’s best based on your mood. No need to wonder around for hours, looking for something you haven’t seen and that seems to fit how you’re feeling tonight.",
-      photos: ["s-1.png"]
+      text2: "To allow the audience to understand a little bit better how this robot works, we projected a design that evolves by the calculation from the robot position to the nearest obstacle, creating a sine curve : red when close to an obstacle, blue when far from it.",
+      photos: ["s-1.jpg", "s-2.jpg", "s-3.jpg", "s-4.jpg"]
     },
     {
       id: "attraction-magnitude",
       title: "Attraction Magnitude",
       type: "Data visualization",
       date: "2014",
-      background: "lost-elements.png",
+      background: "am-cover.jpg",
       details: {
-        what: ["IoT", "Arduino"],
-        when: "2017",
-        who: ["Léo Ternoir", "Jacky Fong", "Léna Scaravella", "Thomas Boulongne", "Sofia Boggio"]
+        what: ["Data visualization"],
+        when: "2015",
+        who: ["Jacky Fong", "Sofia Boggio"]
       },
-      text1: "Cushy! is a cushion that you can squeeze, pet, or punch when you’re cozy, on your couch, and you don’t know what TV show or movie to watch. It’ll tell you what’s best based on your mood. No need to wonder around for hours, looking for something you haven’t seen and that seems to fit how you’re feeling tonight.",
-      vimeoId: "211794682",
-      text2: "Cushy! is a cushion that you can squeeze, pet, or punch when you’re cozy, on your couch, and you don’t know what TV show or movie to watch. It’ll tell you what’s best based on your mood. No need to wonder around for hours, looking for something you haven’t seen and that seems to fit how you’re feeling tonight.",
-      photos: ["poule.jpg"]
+      text1: "Just because something isn’t tangible doesn’t mean you can’t see it. Data visualization has this power to make informations understandable in a glance. As Gobelins school has a lot of social medias, we created an experience that allows everyone to notice some patterns in the school’s visibility, the number of people following them or viewing their videos.",
+      text2: "https://jackyfong.com/lab/socialnetworksactivity/",
+      photos: ["am-0.gif", "am-1.jpg", "am-2.jpg"]
     }
   ];
