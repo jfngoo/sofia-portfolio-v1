ab1b6ddcabb4e81f0148f7eaa36a20f15683bcb5
Jacky Fong <jacky.fong@afp.com>
2017-04-11 14:57:58 +0200

    [FIX] Reduced deltaY needed

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,173 +1,173 @@
 <template>
-  <div id="intro">
+    <div id="intro">
 
-    <borders ref="borders" id="borders"></borders>
+        <borders ref="borders" id="borders"></borders>
 
-    <div id="intro-wrapper">
-      <div ref="title" class="center">
-        <h1>sofia boggio</h1>
-        <img src="../assets/svg/logo-sofia.svg">
-      </div>
-      <div ref="scroll" id="scrollto">
-        <router-link to="home">scroll to discover</router-link>
-      </div>
-      <div ref="bar" id="bar"></div>
+        <div id="intro-wrapper">
+            <div ref="title" class="center">
+                <h1>sofia boggio</h1>
+                <img src="../assets/svg/logo-sofia.svg">
+            </div>
+            <div ref="scroll" id="scrollto">
+                <router-link to="home">scroll to discover</router-link>
+            </div>
+            <div ref="bar" id="bar"></div>
+        </div>
+
     </div>
-
-  </div>
 </template>
 
 <script>
 
-  import _ from 'lodash'
-  import {TweenMax} from 'gsap'
-  import StateManager from 'lib/stateManager'
+    import _ from 'lodash'
+    import {TweenMax} from 'gsap'
+    import StateManager from 'lib/stateManager'
 
-  import Borders from 'components/Borders'
+    import Borders from 'components/Borders'
 
-  export default {
-    name: 'IntroComponent',
+    export default {
+        name: 'IntroComponent',
 
-    components: {
-        Borders,
-    },
+        components: {
+            Borders,
+        },
 
-    mounted() {
-      this.blockScroll();
-      this.launched = false;
-      this.addEventListeners();
-      this.setTweens();
-    },
+        mounted() {
+            this.blockScroll();
+            this.launched = false;
+            this.addEventListeners();
+            this.setTweens();
+        },
 
-    beforeDestroy() {
-      this.removeEventListeners();
-      this.unblockScroll();
-    },
+        beforeDestroy() {
+            this.removeEventListeners();
+            this.unblockScroll();
+        },
 
-    methods: {
-      goTo() {
-        StateManager.setPlayHomeAnimation(true);
-        this.$router.push({name: 'home'});
-      },
+        methods: {
+            goTo() {
+                StateManager.setPlayHomeAnimation(true);
+                this.$router.push({name: 'home'});
+            },
 
-      blockScroll() {
-        window.scrollTo(0, 0);
-        document.querySelector('body').style.overflow = "hidden";
-        document.querySelector('html').style.overflow = "hidden";
-      },
+            blockScroll() {
+                window.scrollTo(0, 0);
+                document.querySelector('body').style.overflow = "hidden";
+                document.querySelector('html').style.overflow = "hidden";
+            },
 
-      unblockScroll() {
-        document.querySelector('body').style.overflow = "auto";
-        document.querySelector('html').style.overflow = "auto";
-      },
+            unblockScroll() {
+                document.querySelector('body').style.overflow = "auto";
+                document.querySelector('html').style.overflow = "auto";
+            },
 
-      addEventListeners() {
-        this.scrollListener = _.throttle(this.onScroll, 200);
+            addEventListeners() {
+                this.scrollListener = _.throttle(this.onScroll, 200);
 
-        window.addEventListener("mousewheel", this.scrollListener);
-        window.addEventListener("touchmove", this.scrollListener);
-      },
+                window.addEventListener("mousewheel", this.scrollListener);
+                window.addEventListener("touchmove", this.scrollListener);
+            },
 
-      removeEventListeners() {
-        window.removeEventListener("mousewheel", this.scrollListener);
-        window.removeEventListener("touchmove", this.scrollListener);
-      },
+            removeEventListeners() {
+                window.removeEventListener("mousewheel", this.scrollListener);
+                window.removeEventListener("touchmove", this.scrollListener);
+            },
 
-      setTweens() {
-          TweenMax.from(this.$refs.title, 1, {y: 50, opacity: 0});
-          TweenMax.from(this.$refs.bar, 1, {y: 50, opacity: 0, delay: .5});
-          TweenMax.from(this.$refs.scroll, 1, {opacity: 0, delay: 1.5});
-      },
+            setTweens() {
+                TweenMax.from(this.$refs.title, 1, {y: 50, opacity: 0});
+                TweenMax.from(this.$refs.bar, 1, {y: 50, opacity: 0, delay: .5});
+                TweenMax.from(this.$refs.scroll, 1, {opacity: 0, delay: 1.5});
+            },
 
-      onScroll(e) {
-        if (!this.launched && (e.deltaY > 10 || e.changedTouches)) {
-          this.launched = true;
+            onScroll(e) {
+                if (!this.launched && (e.deltaY > 4 || e.changedTouches)) {
+                    this.launched = true;
 
-          TweenMax.killAll();
+                    TweenMax.killAll();
 
-          TweenMax.set(this.$refs.title, {y: 0, opacity: 1});
-          TweenMax.set(this.$refs.bar, {opacity: 1});
+                    TweenMax.set(this.$refs.title, {y: 0, opacity: 1});
+                    TweenMax.set(this.$refs.bar, {opacity: 1});
 
-          let tl = new TimelineMax({onComplete: this.goTo});
+                    let tl = new TimelineMax({onComplete: this.goTo});
 
-          tl.to(this.$refs.title, 2, {y: -1000, opacity: 0, ease: Power3.easeIn}, "tag");
-          tl.to(this.$refs.scroll, .5, {y: -10, opacity: 0}, "tag");
-          tl.to(this.$refs.bar, 2, {height: "120vh", ease: Power3.easeIn}, "tag");
-          tl.to(this.$refs.bar, 1, {scaleX: 1000});
-          tl.set("#borders", {display: "block"});
-          tl.fromTo("#borders .border-left", .5, {opacity: 0}, {opacity: 1}, "tag2");
-          tl.fromTo("#borders .border-bottom", .5, {opacity: 0}, {opacity: 1}, "tag2");
+                    tl.to(this.$refs.title, 2, {y: -1000, opacity: 0, ease: Power3.easeIn}, "tag");
+                    tl.to(this.$refs.scroll, .5, {y: -10, opacity: 0}, "tag");
+                    tl.to(this.$refs.bar, 2, {height: "120vh", ease: Power3.easeIn}, "tag");
+                    tl.to(this.$refs.bar, 1, {scaleX: "1000"});
+                    tl.set("#borders", {display: "block"});
+                    tl.fromTo("#borders .border-left", .5, {opacity: 0}, {opacity: 1}, "tag2");
+                    tl.fromTo("#borders .border-bottom", .5, {opacity: 0}, {opacity: 1}, "tag2");
+                }
+            },
         }
-      },
     }
-  }
 </script>
 
 <style lang="scss">
 
-  @import '../utils/global';
+    @import '../utils/global';
 
-  #intro {
-    height: 100vh;
-    width: 100vw;
+    #intro {
+        height: 100vh;
+        width: 100vw;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-    background: $clear_purple;
+        background: $clear_purple;
 
-    #borders {
-      position: absolute;
-      height: 100vh;
-      width: 100vw;
-      display: none;
-      top: 0;
-      left: 0;
-    }
-
-    #intro-wrapper {
-
-      .center {
-        transform: rotate(-90deg);
-      }
-
-      h1 {
-        display: inline-block;
-      }
-
-      img {
-        display: inline-block;
-        height: 20px;
-        margin-left: 20px;
-      }
-
-      #scrollto {
-        font-size: .8rem;
-        position: absolute;
-        bottom: 50px;
-        left: 50%;
-        transform: translateX(-50%);
-        text-align: center;
-
-        a {
-          text-decoration: none;
-          color: $dark_purple;
+        #borders {
+            position: absolute;
+            height: 100vh;
+            width: 100vw;
+            display: none;
+            top: 0;
+            left: 0;
         }
-      }
 
-      #bar {
-        height: 40px;
-        width: 4px;
-        background: $dark_purple;
-        position: absolute;
-        bottom: 0;
-        display: block;
-        left: calc(50% - 2px);
-      }
+        #intro-wrapper {
+
+            .center {
+                transform: rotate(-90deg);
+            }
+
+            h1 {
+                display: inline-block;
+            }
+
+            img {
+                display: inline-block;
+                height: 20px;
+                margin-left: 20px;
+            }
+
+            #scrollto {
+                font-size: .8rem;
+                position: absolute;
+                bottom: 50px;
+                left: 50%;
+                transform: translateX(-50%);
+                text-align: center;
+
+                a {
+                    text-decoration: none;
+                    color: $dark_purple;
+                }
+            }
+
+            #bar {
+                height: 40px;
+                width: 4px;
+                background: $dark_purple;
+                position: fixed;
+                bottom: 0;
+                display: block;
+                left: calc(50% - 2px);
+            }
+
+        }
 
     }
-
-  }
 
 </style>
