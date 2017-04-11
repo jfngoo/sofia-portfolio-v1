b10690fccdce6b7443dd381ed74202730a3e696e
Jacky Fong <jacky.fong@afp.com>
2017-04-11 15:46:06 +0200

    [STYLE/CORE] Add hover on nav, remove "archives"

--- src/components/Borders.vue
+++ src/components/Borders.vue
@@ -1,125 +1,144 @@
 <template>
 
-  <div id="borders">
-    <div class="border-top horizontal-border"></div>
-    <div class="border-right vertical-border"></div>
-    <div class="border-bottom horizontal-border">
-      © Sofia Boggio - 2017
+    <div id="borders">
+        <div class="border-top horizontal-border"></div>
+        <div class="border-right vertical-border"></div>
+        <div class="border-bottom horizontal-border">
+            © Sofia Boggio - 2017
 
+
+        </div>
+        <div class="border-left vertical-border">
+            <img class="logo" src="../assets/svg/logo-sofia-clear.svg" alt="" @click="goToHome()">
+            <nav>
+                <ul>
+                    <li>about & contact</li>
+                    <!--<li>archive</li>-->
+                    <li @click="goToHome()">work</li>
+                </ul>
+            </nav>
+        </div>
     </div>
-    <div class="border-left vertical-border">
-      <img class="logo" src="../assets/svg/logo-sofia-clear.svg" alt="" @click="goToHome()">
-      <nav>
-        <ul>
-          <li>about & contact</li>
-          <li>archive</li>
-          <li @click="goToHome()">work</li>
-        </ul>
-      </nav>
-    </div>
-  </div>
 
 </template>
 
 <script>
 
-  import StateManager from 'lib/stateManager'
-  import EventBus from 'lib/eventBus'
+    import StateManager from 'lib/stateManager'
+    import EventBus from 'lib/eventBus'
 
-  export default {
-    name: 'BordersComponent',
-    data () {
-      return {
-        projects: null
-      }
-    },
+    export default {
+        name: 'BordersComponent',
+        data () {
+            return {
+                projects: null
+            }
+        },
 
-    methods: {
-      goToHome() {
-        if (StateManager.getIsInProject()) {
-          EventBus.$emit("BACK_TO_HOME");
-        } else {
-          this.$router.push({name: "home"});
+        methods: {
+            goToHome() {
+                if (StateManager.getIsInProject()) {
+                    EventBus.$emit("BACK_TO_HOME");
+                } else {
+                    this.$router.push({name: "home"});
+                }
+            }
         }
-      }
     }
-  }
 </script>
 
 <style lang="scss">
 
-  @import '../utils/global.scss';
+    @import '../utils/global.scss';
 
-  #borders {
+    #borders {
 
-    color: $clear-purple;
-    font-size: .9rem;
+        color: $clear-purple;
+        font-size: .9rem;
 
-    .horizontal-border {
-      height: $border_width;
-      width: 100vw;
-      position: fixed;
-      left: 0;
-      z-index: 99;
-      background: $background_color;
-    }
-    .vertical-border {
-      height: 100vh;
-      width: 60px;
-      position: fixed;
-      top: 0;
-      z-index: 99;
-      background: $background_color;
-    }
-    .border-left {
-      left: 0;
-      .logo {
-        position: relative;
-        z-index: 99999;
-        transform: rotate(-90deg);
-        margin-top: 83px;
-        &:hover {
-          cursor: pointer;
+        .horizontal-border {
+            height: $border_width;
+            width: 100vw;
+            position: fixed;
+            left: 0;
+            z-index: 99;
+            background: $background_color;
         }
-      }
-
-      nav {
-
-        transform: rotate(-90deg);
-        position: absolute;
-        left: 30px;
-        bottom: 10px;
-        width: 100vw;
-        transform-origin: 0;
-        z-index: 999;
-        text-align: left;
-        ul {
-          margin: 0;
-          li {
-            display: inline-block;
-            &:hover {
-              cursor: pointer;
+        .vertical-border {
+            height: 100vh;
+            width: 60px;
+            position: fixed;
+            top: 0;
+            z-index: 99;
+            background: $background_color;
+        }
+        .border-left {
+            left: 0;
+            .logo {
+                position: relative;
+                z-index: 99999;
+                transform: rotate(-90deg);
+                margin-top: 83px;
+                &:hover {
+                    cursor: pointer;
+                }
             }
-          }
 
-          li + li {
-            margin-left: 30px;
-          }
+            nav {
+
+                transform: rotate(-90deg);
+                position: absolute;
+                left: 30px;
+                bottom: 10px;
+                width: 100vw;
+                transform-origin: 0;
+                z-index: 999;
+                text-align: left;
+                ul {
+                    margin: 0;
+                    li {
+                        position: relative;
+                        display: inline-block;
+
+                        &:after {
+                            content: '';
+                            width: 0;
+                            height: 2px;
+                            background: #FFFFFF;
+                            position: absolute;
+                            left: 50%;
+                            transform: translateX(-50%);
+                            bottom: -4px;
+                            transition: width ease .4s;
+                        }
+
+                        &:hover {
+                            cursor: pointer;
+                        }
+
+                        &:hover:after {
+                            width: 80%;
+                        }
+                    }
+
+                    li + li {
+                        margin-left: 30px;
+                    }
+                }
+            }
+
         }
-      }
+        .border-right {
+            right: 0;
+        }
+        .border-top {
+            top: 0;
+        }
+        .border-bottom {
+            bottom: 0;
 
+            line-height: $border_width;
+        }
     }
-    .border-right {
-      right: 0;
-    }
-    .border-top {
-      top: 0;
-    }
-    .border-bottom {
-      bottom: 0;
-
-      line-height: $border_width;
-    }
-  }
 
 </style>
