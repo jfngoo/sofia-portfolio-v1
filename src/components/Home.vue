5428ede80a61e25b13ecfa07a6e4089203430e36
Jacky Fong <jacky.fong@afp.com>
2017-04-11 15:32:50 +0200

    [CORE] Optimized home enter animation, speed up animations

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,229 +1,230 @@
 <template>
-  <div ref="home" id="home">
-    <borders></borders>
+    <div ref="home" id="home">
+        <borders></borders>
 
-    <div ref="mask" id="mask"></div>
+        <div ref="mask" id="mask"></div>
 
-    <div ref="projects" id="projects">
-      <ul>
-        <li v-for="project in projects" :id="project.id"
-            :style="{ backgroundImage: 'url(' + getCover(project.background) +')' }"
-            @click="goToProject(project.id, $event)">
-          <div class="wrapper">
-            <div class="title">{{project.title}}</div>
-            <div class="type">{{project.type}}</div>
-            <div class="date">{{project.date}}</div>
-          </div>
-        </li>
-      </ul>
+        <div ref="projects" id="projects">
+            <ul>
+                <li v-for="project in projects" :id="project.id"
+                    :style="{ backgroundImage: 'url(' + getCover(project.background) +')' }"
+                    @click="goToProject(project.id, $event)">
+                    <div class="wrapper">
+                        <div class="title">{{project.title}}</div>
+                        <div class="type">{{project.type}}</div>
+                        <div class="date">{{project.date}}</div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
     </div>
-
-  </div>
 </template>
 
 <script>
 
-  import Borders from 'components/Borders'
-  import DataManager from 'lib/dataManager'
-  import AssetsManager from 'lib/assetsManager'
-  import StateManager from 'lib/stateManager'
-  import EventBus from 'lib/eventBus'
+    import Borders from 'components/Borders'
+    import DataManager from 'lib/dataManager'
+    import AssetsManager from 'lib/assetsManager'
+    import StateManager from 'lib/stateManager'
+    import EventBus from 'lib/eventBus'
 
-  import {TweenMax} from 'gsap'
-  import ScrollToPlugin from 'gsap/ScrollToPlugin';
+    import {TweenMax} from 'gsap'
+    import ScrollToPlugin from 'gsap/ScrollToPlugin';
 
-  export default {
-    name: 'HomeComponent',
+    export default {
+        name: 'HomeComponent',
 
-    components: {
-      Borders,
-    },
+        components: {
+            Borders,
+        },
 
-    data () {
-      return {
-        projects: null
-      }
-    },
-
-    mounted() {
-      this.resetScroll();
-      this.projects = DataManager.getProjects();
-      this.$nextTick(this.setTweens);
-    },
-
-    methods: {
-      resetScroll() {
-        window.scrollTo(0, 0);
-      },
-
-      getCover(fileName) {
-        return AssetsManager.getCover(fileName);
-      },
-
-      goToProject(id, event) {
-
-        const target = event.target.offsetTop - 60;
-
-        const height = this.$refs.projects.offsetHeight * 1.5;
-        TweenMax.set(this.$refs.projects, {height: height});
-
-        const offset = target - window.scrollY;
-        let speed = .5;
-
-        if (offset < 100) {
-          speed = .25;
-        } else if (offset === 0) {
-            speed = .00001;
-        }
-
-        TweenMax.to(window, speed, {
-          scrollTo: {
-            y: target,
-            onAutoKill: () => {
-              TweenMax.set(this.$refs.projects, {height: "auto"});
+        data () {
+            return {
+                projects: null
             }
-          }, onComplete: () => {
-            this.$router.push({name: "project", params: {id: id}});
-          }
-        });
+        },
 
-      },
+        mounted() {
+            this.resetScroll();
+            this.projects = DataManager.getProjects();
+            this.$nextTick(this.setTweens);
+        },
 
-      setTweens() {
+        methods: {
+            resetScroll() {
+                window.scrollTo(0, 0);
+            },
+
+            getCover(fileName) {
+                return AssetsManager.getCover(fileName);
+            },
+
+            goToProject(id, event) {
+
+                const target = event.target.offsetTop - 60;
+
+                const height = this.$refs.projects.offsetHeight * 1.5;
+                TweenMax.set(this.$refs.projects, {height: height});
+
+                const offset = target - window.scrollY;
+                let speed = .5;
+
+                if (offset < 100) {
+                    speed = .25;
+                } else if (offset === 0) {
+                    speed = .00001;
+                }
+
+                TweenMax.to(window, speed, {
+                    scrollTo: {
+                        y: target,
+                        onAutoKill: () => {
+                            TweenMax.set(this.$refs.projects, {height: "auto"});
+                        }
+                    }, onComplete: () => {
+                        this.$router.push({name: "project", params: {id: id}});
+                    }
+                });
+
+            },
+
+            setTweens() {
 
 
-        const tl = new TimelineMax();
+                const tl = new TimelineMax();
 
-        if (StateManager.getPlayHomeAnimation()) {
+                if (StateManager.getPlayHomeAnimation()) {
 
 
-          tl.set(this.$refs.mask, {width: 1});
-          tl.to(this.$refs.mask, 1, {scale: 5000, ease: Power3.easeIn});
+                    tl.set(this.$refs.mask, {width: 1});
+//          tl.to(this.$refs.mask, 1, {scale: 5000, ease: Power3.easeIn});
+                    tl.to(this.$refs.mask, .5, {width: "180vw", ease: Power3.easeIn});
 
-          tl.set(this.$refs.home, {background: "#F0F2FA"});
-          tl.set(this.$refs.mask, {display: "none"});
-          tl.set(this.$refs.projects, {visibility: "visible"});
-          tl.staggerFrom("#projects ul li", 1, {opacity: 0, x: 40}, .25, "tag");
-          tl.staggerFrom("#projects ul li .wrapper", 1, {opacity: 0, x: 80}, .25, "tag -= 1.5");
+                    tl.set(this.$refs.home, {background: "#F0F2FA"});
+                    tl.set(this.$refs.mask, {display: "none"});
+                    tl.set(this.$refs.projects, {visibility: "visible"});
+                    tl.staggerFrom("#projects ul li", 1, {opacity: 0, x: 40}, .25, "tag");
+                    tl.staggerFrom("#projects ul li .wrapper", 1, {opacity: 0, x: 80}, .25, "tag -= 1.5");
 
-          StateManager.setPlayHomeAnimation(false);
-        } else if (StateManager.getIsInProject()) {
+                    StateManager.setPlayHomeAnimation(false);
+                } else if (StateManager.getIsInProject()) {
 
-          tl.set(this.$refs.home, {background: "#F0F2FA"});
-          tl.set(this.$refs.mask, {display: "none"});
-          tl.set(this.$refs.projects, {visibility: "visible"});
-          const active = document.getElementById(StateManager.getIsInProject())
-          const scrollTarget = active.offsetTop - 60;
-          window.scrollTo(0, scrollTarget);
-          tl.set("#projects ul li", {opacity: 0});
-          tl.set(active, {opacity: 1});
-          tl.to("#projects ul li", 1, {opacity: 1});
-          tl.staggerFrom("#projects ul li .wrapper", 1, {opacity: 0, x: 80}, .25, "tag -= 1.5");
+                    tl.set(this.$refs.home, {background: "#F0F2FA"});
+                    tl.set(this.$refs.mask, {display: "none"});
+                    tl.set(this.$refs.projects, {visibility: "visible"});
+                    const active = document.getElementById(StateManager.getIsInProject())
+                    const scrollTarget = active.offsetTop - 60;
+                    window.scrollTo(0, scrollTarget);
+                    tl.set("#projects ul li", {opacity: 0});
+                    tl.set(active, {opacity: 1});
+                    tl.to("#projects ul li", 1, {opacity: 1});
+                    tl.staggerFrom("#projects ul li .wrapper", 1, {opacity: 0, x: 80}, .25, "tag -= 1.5");
 
-          StateManager.setIsInProject(null);
+                    StateManager.setIsInProject(null);
+                }
+                else {
+                    tl.set(this.$refs.home, {background: "#F0F2FA"});
+                    tl.set(this.$refs.mask, {display: "none"});
+                    tl.set(this.$refs.projects, {visibility: "visible"});
+                    tl.staggerFrom("#projects ul li .wrapper", 1, {opacity: 0, x: 80}, .25, "tag -= 1.5");
+                }
+
+
+            }
         }
-        else {
-          tl.set(this.$refs.home, {background: "#F0F2FA"});
-          tl.set(this.$refs.mask, {display: "none"});
-          tl.set(this.$refs.projects, {visibility: "visible"});
-          tl.staggerFrom("#projects ul li .wrapper", 1, {opacity: 0, x: 80}, .25, "tag -= 1.5");
-        }
-
-
-      }
     }
-  }
 </script>
 
 <style lang="scss">
 
-  @import '../utils/global';
+    @import '../utils/global';
 
-  #home {
-    min-height: 100vh;
-    background: $dark_purple;
-    overflow-x: hidden;
-    padding: 0 60px;
-
-    #mask {
-      position: fixed;
-      top: 0;
-      left: 50%;
-      height: 100vh;
-      width: 0;
-      background: $clear_purple;
-      z-index: 10;
-    }
-
-    #projects {
-      visibility: hidden;
-
-      ul {
-        padding: 60px 0 60px 0;
-        margin: 0 auto;
-        width: 100%;
-
-        li {
-          position: relative;
-          margin: 0 auto;
-          display: block;
-          width: 100%;
-          height: 500px;
-          background: #FFFFFF center center no-repeat;
-          background-size: cover;
-          transition: .25s filter ease;
-          filter: grayscale(100%);
-
-          &:hover {
-            filter: grayscale(0%);
-            cursor: pointer;
-          }
-
-          .wrapper {
-            position: absolute;
-            bottom: 100px;
-            left: 100px;
-            color: #FFFFFF;
-            text-align: left;
-            text-shadow: 1px 1px 10px rgba(0, 0, 0, .5);
-
-            .title {
-              font-size: 2.4rem;
-              text-transform: uppercase;
-            }
-
-            .type {
-              margin: -4px 0 2px 0;
-            }
-
-            .date {
-              font-size: 1.1rem;
-            }
-          }
-
-        }
-
-        li + li {
-          /*margin-top: 20px;*/
-        }
-      }
-    }
-  }
-
-
-  @media all and (max-width: 570px) {
     #home {
-      #projects {
-        ul li .wrapper {
-          bottom: 20px;
-          left: 20px;
+        min-height: 100vh;
+        background: $dark_purple;
+        overflow-x: hidden;
+        padding: 0 60px;
 
-          .title {
-            font-size: 8vw;
-          }
+        #mask {
+            position: fixed;
+            top: 0;
+            left: 50%;
+            transform: translateX(-50%);
+            height: 100vh;
+            width: 0;
+            background: $clear_purple;
+            z-index: 10;
         }
-      }
+
+        #projects {
+            visibility: hidden;
+
+            ul {
+                padding: 60px 0 60px 0;
+                margin: 0 auto;
+                width: 100%;
+
+                li {
+                    position: relative;
+                    margin: 0 auto;
+                    display: block;
+                    width: 100%;
+                    height: 500px;
+                    background: #FFFFFF center center no-repeat;
+                    background-size: cover;
+                    transition: .25s filter ease;
+                    filter: grayscale(100%);
+
+                    &:hover {
+                        filter: grayscale(0%);
+                        cursor: pointer;
+                    }
+
+                    .wrapper {
+                        position: absolute;
+                        bottom: 100px;
+                        left: 100px;
+                        color: #FFFFFF;
+                        text-align: left;
+                        text-shadow: 1px 1px 10px rgba(0, 0, 0, .5);
+
+                        .title {
+                            font-size: 2.4rem;
+                            text-transform: uppercase;
+                        }
+
+                        .type {
+                            margin: -4px 0 2px 0;
+                        }
+
+                        .date {
+                            font-size: 1.1rem;
+                        }
+                    }
+
+                }
+
+                li + li {
+                    /*margin-top: 20px;*/
+                }
+            }
+        }
     }
-  }
+
+    @media all and (max-width: 570px) {
+        #home {
+            #projects {
+                ul li .wrapper {
+                    bottom: 20px;
+                    left: 20px;
+
+                    .title {
+                        font-size: 8vw;
+                    }
+                }
+            }
+        }
+    }
 
 </style>
--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -93,7 +93,7 @@
                     tl.to(this.$refs.title, 2, {y: -1000, opacity: 0, ease: Power3.easeIn}, "tag");
                     tl.to(this.$refs.scroll, .5, {y: -10, opacity: 0}, "tag");
                     tl.to(this.$refs.bar, 2, {height: "120vh", ease: Power3.easeIn}, "tag");
-                    tl.to(this.$refs.bar, 1, {width: "100vw"});
+                    tl.to(this.$refs.bar, .5, {width: "100vw"});
                     tl.set("#borders", {display: "block"});
                     tl.fromTo("#borders .border-left", .5, {opacity: 0}, {opacity: 1}, "tag2");
                     tl.fromTo("#borders .border-bottom", .5, {opacity: 0}, {opacity: 1}, "tag2");
